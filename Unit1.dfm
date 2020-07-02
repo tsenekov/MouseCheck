b924edfec6717725c28d2b05b75fbbf7e5faf835
alexey@tsenekov.ru <tsenekov>
2020-07-02 20:58:45 +0300

    Up

---
+++ Unit1.dfm
@@ -0,0 +1,40 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Hit numlock'
+  ClientHeight = 240
+  ClientWidth = 565
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnClose = FormClose
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Memo1: TMemo
+    AlignWithMargins = True
+    Left = 3
+    Top = 3
+    Width = 559
+    Height = 234
+    Align = alClient
+    Lines.Strings = (
+      'Memo1')
+    ScrollBars = ssVertical
+    TabOrder = 0
+    ExplicitWidth = 629
+    ExplicitHeight = 293
+  end
+  object Timer1: TTimer
+    Enabled = False
+    Interval = 10
+    OnTimer = Timer1Timer
+    Left = 16
+    Top = 24
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,75 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, System.DateUtils;
+
+type
+  TForm1 = class(TForm)
+    Timer1: TTimer;
+    Memo1: TMemo;
+    procedure FormShow(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    { Private declarations }
+  public
+    sd: TTime;
+    ls: integer;
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+    Memo1.Lines.SaveToFile('d:\temp.txt');
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Memo1.Clear;
+  sd := now();
+  Timer1.Enabled := true;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+var
+  ks: integer;
+  r: string;
+  t: int64;
+begin
+  if GetKeyState(vk_NumLock)=1 then
+  begin
+    ks := GetKeyState(1);
+    if ks <> ls then
+    begin
+      ls := ks;
+
+      t := MilliSecondsBetween(now(), sd);
+      sd := now();
+      Memo1.Lines.Add('Delay ' + t.ToString + ' ms');
+
+      if ks < 0 then
+      begin
+        Memo1.Lines.Add('MoveTo ' + Mouse.CursorPos.X.ToString + ' ' + Mouse.CursorPos.Y.ToString);
+      end;
+
+      if ks < 0 then
+        r := 'LeftDown 1';
+      if ks >= 0 then
+        r := 'LeftUp 1';
+
+      Memo1.Lines.Add(r);
+    end;
+  end;
+end;
+
+end.
